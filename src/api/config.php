6032471d7c4cbcec952e038a492b5b4087b4c0eb
NS <nik@marketingeducation.org>
2024-02-10 21:48:27 +0800

    load .env

--- src/api/config.php
+++ src/api/config.php
@@ -9,7 +9,12 @@
  * @link   https://github.com/nikslab/niks-mailerlite
  */
 
-// Database
+// Load environment variables if they are not loaded already
+if (!getenv('MYSQL_DATABASE')) {
+    loadEnv('../../.env');
+}
+
+ // Database
 $dbHost = getenv('MYSQL_HOSTNAME');
 $dbName = getenv('MYSQL_DATABASE');
 $dbUser = getenv('MYSQL_USER');
@@ -28,7 +33,44 @@ try {
     exit(0);
 }
 
-// Function to sanitize input and prevent SQL injection
+/**
+ * Loads environmental variables if they are not already loaded
+ * 
+ * PHP Version 8.0
+ * 
+ * @param $filePath file path to .env file
+ * 
+ * @return true
+ */
+function loadEnv($filePath = '.env') {
+    if (!file_exists($filePath)) {
+        return false; // .env file not found
+    }
+
+    $lines = file($filePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+    foreach ($lines as $line) {
+        // Ignore comments
+        if (strpos($line, '#') !== false) {
+            continue;
+        }
+
+        list($key, $value) = explode('=', $line, 2);
+
+        // Trim leading/trailing whitespaces and quotes
+        $key = trim($key);
+        $value = trim($value, " \t\n\r\0\x0B'\"");
+
+        // Set the environment variable
+        putenv("$key=$value");
+        $_ENV[$key] = $value;
+        $_SERVER[$key] = $value;
+    }
+
+    return true;
+}
+
+
 /**
  * PDO connection to database and some functions
  * 
